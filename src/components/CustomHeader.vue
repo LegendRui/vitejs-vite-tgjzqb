7e15d87663ee3e7132557738f62af4c63f6940dd
LegendRui <838433043@qq.com>
2022-02-16 17:36:39 +0800

    Initial commit

---
+++ src/components/CustomHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="custom-header">
+    <div class="avatar">
+      <el-dropdown>
+        <el-avatar :size="30" :src="circleUrl"></el-avatar>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item>用户中心</el-dropdown-item>
+            <el-dropdown-item>Action 2</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+const circleUrl =
+  'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png';
+</script>
+<style lang="less" scoped>
+.custom-header {
+  display: flex;
+  height: 60px;
+  justify-content: flex-end !important;
+  align-content: center;
+  align-items: center;
+}
+.avatar {
+  margin-right: 12px;
+}
+</style>
---
+++ src/components/TestElement.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <CustomHeader />
+      </el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-container>
+          <el-main>Main</el-main>
+        </el-container>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import CustomHeader from './CustomHeader.vue';
+</script>
+
+<style scoped>
+.common-layout .el-header,
+.common-layout .el-footer {
+  background-color: skyblue;
+  color: var(--el-text-color-primary);
+  text-align: center;
+  height: 60px;
+  padding: 0 !important;
+  margin: 0 !important;
+}
+.common-layout .el-footer {
+  line-height: 60px;
+}
+
+.common-layout .el-aside {
+  background-color: #d3dce6;
+  color: var(--el-text-color-primary);
+  text-align: center;
+  position: fixed;
+  top: 60px;
+  height: calc(100% - 60px);
+}
+
+.common-layout .el-main {
+  background-color: #e9eef3;
+  color: var(--el-text-color-primary);
+  text-align: center;
+  position: fixed;
+  top: 60px;
+  left: 200px;
+  width: calc(100% - 200px);
+  height: calc(100% - 60px);
+}
+
+.common-layout > .el-container {
+  margin-bottom: 40px;
+}
+
+.common-layout .el-container:nth-child(5) .el-aside,
+.common-layout .el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.common-layout .el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+</style>
